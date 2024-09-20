bee05bd0a4746857b965c348fce48ce8a89c2e1b
baer1 <videocreator@outlook.de>
2024-09-20 23:15:08 +0200

    Add funny

--- js/index.js
+++ js/index.js
@@ -11,4 +11,33 @@ window.addEventListener("load", () => {
                 location.href = url.toString();
         }
     });
+    window.addEventListener("focus", () => {
+        if (is_april_fools() || Math.random() < 0.04) {
+            let aud = document.createElement("audio");
+            aud.src = "aud/vineboom.ogg";
+            document.body.appendChild(aud);
+            let img = document.createElement("img");
+            img.src = "img/an871k4o1sn51.png";
+            img.classList.add("funny");
+            aud.addEventListener("ended", () => {
+                try {
+                    img.outerHTML = "";
+                }
+                catch (_a) { }
+                aud.outerHTML = "";
+            });
+            aud
+                .play()
+                .then(() => {
+                document.body.appendChild(img);
+            })
+                .catch(() => {
+                aud.dispatchEvent(new Event("ended"));
+            });
+        }
+    });
 });
+function is_april_fools() {
+    let d = new Date();
+    return d.getDate() == 1 && d.getMonth() == 4;
+}
--- js/index.ts
+++ js/index.ts
@@ -8,4 +8,37 @@ window.addEventListener("load", () => {
       else location.href = url.toString();
     }
   });
+
+  window.addEventListener("focus", () => {
+    if (is_april_fools() || Math.random() < 0.04) {
+      let aud = document.createElement("audio");
+      aud.src = "aud/vineboom.ogg";
+      document.body.appendChild(aud);
+
+      let img = document.createElement("img");
+      img.src = "img/an871k4o1sn51.png";
+      img.classList.add("funny");
+
+      aud.addEventListener("ended", () => {
+        try {
+          img.outerHTML = "";
+        } catch {}
+        aud.outerHTML = "";
+      });
+
+      aud
+        .play()
+        .then(() => {
+          document.body.appendChild(img);
+        })
+        .catch(() => {
+          aud.dispatchEvent(new Event("ended"));
+        });
+    }
+  });
 });
+
+function is_april_fools(): boolean {
+  let d = new Date();
+  return d.getDate() == 1 && d.getMonth() == 4;
+}
