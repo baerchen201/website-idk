17a540ceebcddb8f3d441a5d36472431ea987169
baer1 <videocreator@outlook.de>
2024-09-23 20:21:01 +0200

    Create YouTube video display system

--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,51 @@
+class youtubeVideo extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    let thumbnail: HTMLImageElement = document.createElement("img");
+    let channel_icon: HTMLImageElement = document.createElement("img");
+    let title: HTMLSpanElement = document.createElement("span");
+    let channel: HTMLSpanElement = document.createElement("span");
+
+    let data_id: string | null = this.getAttribute("data-id");
+    let data_thumbnail: string | null = this.getAttribute("data-thumbnail");
+    let data_channel_icon: string | null =
+      this.getAttribute("data-channel-icon");
+    let data_title: string | null = this.getAttribute("data-title");
+    let data_channel: string | null = this.getAttribute("data-channel");
+
+    if (!data_id)
+      throw new Error(
+        `Empty attribute data-id for youtube-video object ${this}`
+      );
+    if (!data_thumbnail) data_thumbnail = "img/youtube-thumbnail.jpg";
+    if (!data_channel_icon) data_channel_icon = "img/youtube-channel-icon.webp";
+    if (!data_title) data_title = "";
+    if (!data_channel) data_channel = "";
+
+    let anchor: HTMLAnchorElement = document.createElement("a");
+    anchor.href = `https://youtu.be/${data_id}`;
+    anchor.title = `Watch ${data_title ? data_title : "video"} on YouTube`;
+    this.appendChild(anchor);
+
+    thumbnail.src = data_thumbnail
+      .replace("%id", data_id)
+      .replace("%s", `https://img.youtube.com/vi/${data_id}/`);
+    channel_icon.src = data_channel_icon;
+    title.innerText = data_title;
+    channel.innerText = data_channel;
+
+    anchor.appendChild(thumbnail);
+    anchor.appendChild(channel_icon);
+    anchor.appendChild(title);
+    anchor.appendChild(channel);
+  }
+}
+
+window.customElements.define("youtube-video", youtubeVideo);
+
 window.addEventListener("load", () => {
   document.getElementById("google")!.addEventListener("keypress", (e) => {
     console.log(e);
