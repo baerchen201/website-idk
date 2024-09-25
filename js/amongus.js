1fd74fedeffd072aa6e457f146aeb7c32a587926
baer1 <videocreator@outlook.de>
2024-09-26 00:44:32 +0200

    Add more amongus-star logic

--- js/amongus.js
+++ js/amongus.js
@@ -5,13 +5,15 @@ class AmongUsBg extends HTMLElement {
     }
     connectedCallback() {
         console.log(this);
-        let free = 50;
+        let free = 125;
         let _ = () => {
             if (free > 0) {
                 free--;
                 let star = document.createElement("amongus-star");
                 console.log(star);
-                star._bg().then(() => {
+                star
+                    ._bg(Math.floor(Math.random() * 2250 + 3000), Math.floor(Math.random() * 50 + 50), Math.floor(Math.random() * 2250 + 3000))
+                    .then(() => {
                     free++;
                     star.remove();
                 });
@@ -26,22 +28,22 @@ class AmongUsStar extends HTMLElement {
     constructor() {
         super();
     }
-    _bg() {
+    _bg(fadein, duration, fadeout) {
         this.style.top = `${Math.random() * 100}%`;
         this.style.left = `${Math.random() * 100}%`;
-        return this.blink(2000, 5000);
+        return this.blink(fadein, duration, fadeout);
     }
-    blink(fade, duration) {
+    blink(fadein, duration, fadeout) {
         return new Promise((resolve, reject) => {
             this.animate([{ opacity: "0" }, { opacity: "1" }], {
                 easing: "ease-out",
-                duration: fade,
+                duration: fadein,
                 fill: "forwards",
             }).addEventListener("finish", () => {
                 setTimeout(() => {
                     this.animate([{ opacity: "1" }, { opacity: "0" }], {
                         easing: "ease-out",
-                        duration: fade,
+                        duration: fadeout,
                         fill: "forwards",
                     }).addEventListener("finish", () => {
                         resolve();
--- js/amongus.ts
+++ js/amongus.ts
@@ -6,7 +6,7 @@ class AmongUsBg extends HTMLElement {
   connectedCallback() {
     console.log(this);
 
-    let free = 50;
+    let free = 125;
     let _ = () => {
       if (free > 0) {
         free--;
@@ -14,10 +14,16 @@ class AmongUsBg extends HTMLElement {
           "amongus-star"
         ) as AmongUsStar;
         console.log(star);
-        star._bg().then(() => {
-          free++;
-          star.remove();
-        });
+        star
+          ._bg(
+            Math.floor(Math.random() * 2250 + 3000),
+            Math.floor(Math.random() * 50 + 50),
+            Math.floor(Math.random() * 2250 + 3000)
+          )
+          .then(() => {
+            free++;
+            star.remove();
+          });
         this.appendChild(star);
       }
       setTimeout(_, Math.floor(Math.random() * 50 + 50));
@@ -31,24 +37,24 @@ class AmongUsStar extends HTMLElement {
     super();
   }
 
-  _bg(): Promise<void> {
+  _bg(fadein: number, duration: number, fadeout: number): Promise<void> {
     this.style.top = `${Math.random() * 100}%`;
     this.style.left = `${Math.random() * 100}%`;
 
-    return this.blink(2000, 5000);
+    return this.blink(fadein, duration, fadeout);
   }
 
-  blink(fade: number, duration: number): Promise<void> {
+  blink(fadein: number, duration: number, fadeout: number): Promise<void> {
     return new Promise((resolve, reject) => {
       this.animate([{ opacity: "0" }, { opacity: "1" }], {
         easing: "ease-out",
-        duration: fade,
+        duration: fadein,
         fill: "forwards",
       }).addEventListener("finish", () => {
         setTimeout(() => {
           this.animate([{ opacity: "1" }, { opacity: "0" }], {
             easing: "ease-out",
-            duration: fade,
+            duration: fadeout,
             fill: "forwards",
           }).addEventListener("finish", () => {
             resolve();
