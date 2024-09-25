aeddff201280043cbbeb494964be5317a69d28ca
baer1 <videocreator@outlook.de>
2024-09-26 00:29:17 +0200

    Add placeholder star animation for amongus-star

--- js/amongus.js
+++ js/amongus.js
@@ -5,6 +5,51 @@ class AmongUsBg extends HTMLElement {
     }
     connectedCallback() {
         console.log(this);
+        let free = 50;
+        let _ = () => {
+            if (free > 0) {
+                free--;
+                let star = document.createElement("amongus-star");
+                console.log(star);
+                star._bg().then(() => {
+                    free++;
+                    star.remove();
+                });
+                this.appendChild(star);
+            }
+            setTimeout(_, Math.floor(Math.random() * 50 + 50));
+        };
+        _();
+    }
+}
+class AmongUsStar extends HTMLElement {
+    constructor() {
+        super();
+    }
+    _bg() {
+        this.style.top = `${Math.random() * 100}%`;
+        this.style.left = `${Math.random() * 100}%`;
+        return this.blink(2000, 5000);
+    }
+    blink(fade, duration) {
+        return new Promise((resolve, reject) => {
+            this.animate([{ opacity: "0" }, { opacity: "1" }], {
+                easing: "ease-out",
+                duration: fade,
+                fill: "forwards",
+            }).addEventListener("finish", () => {
+                setTimeout(() => {
+                    this.animate([{ opacity: "1" }, { opacity: "0" }], {
+                        easing: "ease-out",
+                        duration: fade,
+                        fill: "forwards",
+                    }).addEventListener("finish", () => {
+                        resolve();
+                    });
+                }, duration);
+            });
+        });
     }
 }
 window.customElements.define("amongus-bg", AmongUsBg);
+window.customElements.define("amongus-star", AmongUsStar);
--- js/amongus.ts
+++ js/amongus.ts
@@ -5,7 +5,59 @@ class AmongUsBg extends HTMLElement {
 
   connectedCallback() {
     console.log(this);
+
+    let free = 50;
+    let _ = () => {
+      if (free > 0) {
+        free--;
+        let star: AmongUsStar = document.createElement(
+          "amongus-star"
+        ) as AmongUsStar;
+        console.log(star);
+        star._bg().then(() => {
+          free++;
+          star.remove();
+        });
+        this.appendChild(star);
+      }
+      setTimeout(_, Math.floor(Math.random() * 50 + 50));
+    };
+    _();
+  }
+}
+
+class AmongUsStar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  _bg(): Promise<void> {
+    this.style.top = `${Math.random() * 100}%`;
+    this.style.left = `${Math.random() * 100}%`;
+
+    return this.blink(2000, 5000);
+  }
+
+  blink(fade: number, duration: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.animate([{ opacity: "0" }, { opacity: "1" }], {
+        easing: "ease-out",
+        duration: fade,
+        fill: "forwards",
+      }).addEventListener("finish", () => {
+        setTimeout(() => {
+          this.animate([{ opacity: "1" }, { opacity: "0" }], {
+            easing: "ease-out",
+            duration: fade,
+            fill: "forwards",
+          }).addEventListener("finish", () => {
+            resolve();
+          });
+        }, duration);
+      });
+    });
   }
 }
 
 window.customElements.define("amongus-bg", AmongUsBg);
+window.customElements.define("amongus-star", AmongUsStar);
